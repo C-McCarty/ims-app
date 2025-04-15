419f86dc62e49c15b91d3cad583a313f14e46a30
C-McCarty <cmccarty5@collin.edu>
2025-04-15 18:19:08 -0500

    Full Product functionality established

--- ims-app/src/comp/AddModal.js
+++ ims-app/src/comp/AddModal.js
@@ -2,10 +2,11 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
 
-export default function AddModal({toggleAddModal, addModal, collection, DB_URL}) {
+export default function AddModal({toggleAddModal, addModal, collection, DB_URL, setRefresh}) {
+    // Control input data for Products
     const [prodName, setProdName] = useState("");
     const handleProdNameChange = e => setProdName(e.target.value);
-    const [prodCategoryOption, setProdCategoryOption] = useState(null);
+    const [prodCategoryOption, setProdCategoryOption] = useState(-1);
     const handleSelCategoryChange = e => setProdCategoryOption(e.target.value);
     const [prodNewCategory, setProdNewCategory] = useState("");
     const handleProdNewCategoryChange = e => setProdNewCategory(e.target.value);
@@ -13,86 +14,140 @@ export default function AddModal({toggleAddModal, addModal, collection, DB_URL})
     const handleProdIsTaxableChange = e => setProdIsTaxable(e.target.value);
     const [prodCount, setProdCount] = useState(0);
     const handleProdCountChange = e => setProdCount(e.target.value);
+    const [prodOptionData, setProdOptionData] = useState([]);
+    const [prodOptions, setProdOptions] = useState([]);
 
-    const [optionData, setOptionData] = useState([]);
-    const [options, setOptions] = useState([]);
+    // Control input data for Markets
+    const [markName, setMarkName] = useState("");
+    const handleMarkNameChange = e => setMarkName(e.target.value);
+    const [markDate, setMarkDate] = useState("");
+    const handleMarkDateChange = e => setMarkDate(e.target.value);
+    const [markProds, setMarkProds] = useState([]);
+    const handleMarkProdsChange = e => setMarkProds(e.target.value);
+
+    // Toggle loading screen component
     const [loading, setLoading] = useState(false);
 
+    // Handle form submission for adding a Product or a Market
     const handleSubmit = e => {
         e.preventDefault();
-
-        axios.post(`${DB_URL}/add${collection}`, {
-            body: {
+        // Adding a Product
+        if (collection == "Products") {
+            axios.post(`${DB_URL}/addProducts`, {
                 name: prodName,
                 category: (prodCategoryOption == 0 ? prodNewCategory : prodCategoryOption),
                 isTaxable: (prodIsTaxable == 1 ? true : false),
                 count: prodCount
-            }
-        }).catch(err => {
-            console.error(err);
-        })
+            }).then(()=> {
+                window.alert(`Product "${prodName}" added successfully!`);
+                setProdName("");
+                setProdCategoryOption(null);
+                setProdNewCategory("");
+                setProdIsTaxable(1);
+                setProdCount(0);
+                toggleAddModal(false);
+                setRefresh(true);
+            }).catch(err => {
+                console.error(err);
+                window.alert("An error occurred while trying to add your product. Please try again later.");
+            });
+        }
+        // Adding a Market
+        else {
+            axios.post(`${DB_URL}/addMarkets`, {
+                name: markName,
+                date: markDate,
+                products: markProds
+            }).then(() => {
+                window.alert(`Market "${markName}" added successfully!`);
+                setMarkName("");
+                setMarkDate("");
+                setMarkProds([]);
+                toggleAddModal(false);
+                setRefresh(true);
+            }).catch(err => {
+                console.error(err);
+                window.alert("An error occurred while trying to add your market. Please try again later.");
+            });
+        }
     };
-
+    // Get Product categories to populate category dropdown
     useEffect(() => {
         setLoading(true);
-        axios.get(`${DB_URL}/get${collection}`).then(res => {
-            let list = res.data.map(item => {return item.category});
-            list = new Set(list);
-            list = [...list];
-            setOptionData([...list, 0]);
-        }).catch(err => {
-            console.error(err);
-            setLoading(false);
-        });
+        if (collection == "Products") {
+            axios.get(`${DB_URL}/get${collection}`).then(res => {
+                let list = res.data.map(item => {return item.category});
+                list = new Set(list);
+                list = [...list];
+                setProdOptionData([...list]);
+            }).catch(err => {
+                console.error(err);
+                setLoading(false);
+            });
+        }
     }, [addModal]);
-
+    
+    // Generate <option> elements for category dropdown
     useEffect(() => {
-        console.log(optionData)
-        const list = optionData.map((opt, i) => {
-            return <option value={opt} key={i}>{opt == 0 ? "-- New Category --" : opt}</option>
+        const list = prodOptionData.map((opt, i) => {
+            return <option value={opt} key={i}>{opt}</option>
         });
-        setOptions(list);
+        setProdOptions(list);
         setLoading(false);
-    }, [optionData]);
+    }, [prodOptionData]);
+    
+    const handleClose = () => {
+        setProdName("");
+        setProdCategoryOption(-1);
+        setProdNewCategory("");
+        setProdIsTaxable(1);
+        setProdCount(0);
+        setMarkName("");
+        setMarkDate("");
+        setMarkProds([]);
+        toggleAddModal(false);
+    }
 
     if (addModal) {
         return (
-            <div id="addModalWrap">
-                <div id="addModal">
+            <div id="addModalWrap" className="modalWrap">
+                <div id="addModal" className="modal">
                     <h2>Add {collection == "Products" ? "Product" : "Market"}</h2>
-                    <div className="closeBtn" onClick={()=>toggleAddModal(false)}>
+                    <div className="closeBtn" onClick={handleClose}>
                         <div></div>
                         <div></div>
                     </div>
                     {loading ? <Loading /> :
                         collection == "Products" ?
-                        <form id="addForm" onSubmit={handleSubmit}>
+                        <form id="addForm" className="popupForm" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="prodName">Product Name:</label>
-                                <input type="text" name="prodName" id="prodName" value={prodName} onChange={handleProdNameChange} />
+                                <input type="text" name="prodName" id="prodName" value={prodName} onChange={handleProdNameChange} required />
                             </div>
                             <div>
                                 <label htmlFor="prodCategory">Product Category:</label>
-                                <select name="prodCategory" id="prodCategory" value={prodCategoryOption} onChange={handleSelCategoryChange}>
-                                    {options}
+                                <select name="prodCategory" id="prodCategory" value={prodCategoryOption} onChange={handleSelCategoryChange} required >
+                                    <option value="-1" disabled hidden></option>
+                                    {prodOptions}
+                                    <option value={0}>-- New Category --</option>
                                 </select>
                             </div>
                             {prodCategoryOption == 0 ?
                                 <div>
                                     <label htmlFor="prodNewCategory">New Category:</label>
-                                    <input type="text" name="prodNewCategory" id="prodNewCategory" value={prodNewCategory} onChange={handleProdNewCategoryChange} />
+                                    <input type="text" name="prodNewCategory" id="prodNewCategory" value={prodNewCategory} onChange={handleProdNewCategoryChange} required />
                                 </div>
                             : null}
                             <div>
                                 <label htmlFor="prodTaxable">Taxable?</label>
-                                <select name="prodTaxable" id="prodTaxable" value={prodIsTaxable} onChange={handleProdIsTaxableChange}>
+                                <select name="prodTaxable" id="prodTaxable" value={prodIsTaxable} onChange={handleProdIsTaxableChange} required >
                                     <option value="1">Yes</option>
                                     <option value="0">No</option>
                                 </select>
                             </div>
                             <div>
                                 <label htmlFor="prodCount">Product Count:</label>
-                                <input type="number" id="prodCount" min={0} value={prodCount} onChange={handleProdCountChange} />
+                                <input type="number" id="prodCount" min={0} value={prodCount} onChange={handleProdCountChange} required />
                             </div>
                             <div>
                                 <button type="submit">Submit</button>
--- ims-app/src/comp/ListItem.js
+++ ims-app/src/comp/ListItem.js
@@ -1,33 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EditForm from "./EditForm";
 
-export default function ListItem({collection, name, category=null, taxable=null, count=null, date=null, products=null}) {
+export default function ListItem({collection, DB_URL, name, category=null, taxable=null, count=null, date=null, products=null, _id, setRefresh}) {
+    // Control display of ListItem details
     const [details, toggleDetails] = useState(false);
+    // Control display of editor modal
     const [editForm, toggleEditForm] = useState(false);
-    const [viewProducts, toggleViewProducts] = useState(false);
 
-    return (
-        <div className='listItem'>
-            <div className="listItemMain">
-                <h3>{name}</h3>
-                <div className='details' onClick={()=>{toggleDetails(!details)}}></div>
-                <div className='edit' onClick={()=>{toggleEditForm(true)}}></div>
+    // Product list items
+    if (collection == "Products") {
+        return (
+            <div className='listItem'>
+                <div className="listItemMain">
+                    <h3>{name}</h3>
+                    <div className='details' onClick={()=>{toggleDetails(!details)}}></div>
+                    <div className='edit' onClick={()=>{toggleEditForm(true)}}></div>
+                </div>
+                <div className={details ? "listItemDetails" : "listItemDetails hidden"}>
+                    <div className="category">
+                        <h4>Category</h4>
+                        <p>{category}</p>
+                    </div>
+                    <div className="taxable">
+                        <h4>Taxable?</h4>
+                        <p>{taxable}</p>
+                    </div>
+                    <div className="count">
+                        <h4>Count</h4>
+                        <p>{count}</p>
+                    </div>
+                </div>
+                <EditForm collection={collection} DB_URL={DB_URL} name={name} category={category} taxable={taxable} count={count} editForm={editForm} toggleEditForm={toggleEditForm} _id={_id}  setRefresh={setRefresh}/>
             </div>
-            <div className={details ? "listItemDetails" : "listItemDetails hidden"}>
-                <div className="category">
-                    <h4>Category</h4>
-                    <p>{category}</p>
+        );
+    }
+    // Market list items
+    else if (collection == "Markets") {
+        return (
+            <div className='listItem'>
+                <div className="listItemMain">
+                    <h3>{name}</h3>
+                    <div className='details' onClick={()=>{toggleDetails(!details)}}></div>
+                    <div className='edit' onClick={()=>{toggleEditForm(true)}}></div>
                 </div>
-                <div className="taxable">
-                    <h4>Taxable?</h4>
-                    <p>{taxable}</p>
+                <div className={details ? "listItemDetails" : "listItemDetails hidden"}>
+                    <div className="date">
+                        <h4>Date</h4>
+                        <p>{date}</p>
+                    </div>
+                    <div className="products">
+                        <h4>Products</h4>
+                        {/* I am using <div> elements instead of semantic <table> elements
+                        {/* because they do not support styling the same way. Using <div>
+                        {/* elements also gives me greater control over the layout. */}
+                        <div className="table">
+                            <div className="tr thead">
+
+                                <div className="th">Name</div>
+                                <div className="th">Sent</div>
+                                <div className="th">Left</div>
+                                <div className="th">Sold</div>
+                            </div>
+                            {products.map((p, i) => {
+                                // Generate Product rows
+                                return (
+                                    <div className="tr" key={p._id}>
+                                        <div className="td">{p.name}</div>
+                                        <div className="td">{p.countAllocated}</div>
+                                        <div className="td">{p.countRemaining}</div>
+                                        <div className="td">{p.countAllocated - p.countRemaining}</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
                 </div>
-                <div className="count">
-                    <h4>Count</h4>
-                    <p>{count}</p>
-                </div>
+                <EditForm collection={collection} DB_URL={DB_URL} name={name} date={date} products={products} editForm={editForm} toggleEditForm={toggleEditForm} _id={_id} setRefresh={setRefresh} />
             </div>
-            {editForm ? <EditForm collection={collection} type={2} /> : null}
-        </div>
-    );
+        );
+    }
 }
