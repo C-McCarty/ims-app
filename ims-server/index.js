ce072bf209b0012727ea2c930b47375be26be68f
C-McCarty <cmccarty5@collin.edu>
2025-04-29 20:41:27 -0500

    authentication hotfix

--- ims-server/index.js
+++ ims-server/index.js
@@ -149,8 +149,8 @@ app.put("/updateMarkets", async (req, res) => {
 app.post("/authenticate", async (req, res) => {
     const {USER, PASS} = req.body;
     try {
-        const VALID_AUTH = await MODEL_USER.findOne({USER: USER, PASS: PASS});
-        if (VALID_AUTH) {
+        const CREDENTIALS = await MODEL_USER.findOne({_id: new mongoose.Types.ObjectId(process.env.AUTH_ID)});
+        if (CREDENTIALS.USER === USER && CREDENTIALS.PASS === PASS) {
             res.json({AUTH: true});
         } else {
             res.json({AUTH: false});
